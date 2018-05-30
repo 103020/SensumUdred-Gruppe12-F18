70d7c729f753699a7eb96e7f92852ff0f59a6cbe
103020 <kierkegaardchristian@gmail.com>
2018-05-30 14:31:20 +0200

    remove outcommented methode in Starter

--- SensumUdred/src/Starter/Starter.java
+++ SensumUdred/src/Starter/Starter.java
@@ -10,14 +10,4 @@ public class Starter{
     public static void main(String[] args){
         SensumGUI.main(args);
     }
-
-    /*@Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
-    }*/
 }
